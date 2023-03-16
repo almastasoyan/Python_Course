8bbb3e5f69ee3b8d051b71f1a5c255539360b772
almastasoyan <asoyan.almast@gmail.com>
2023-03-16 20:48:26 +0400

    Addad lesson4.py

---
+++ Week3/lesson4.py
@@ -0,0 +1,46 @@
+x = (4+5)
+print(x)
+
+y = (3+8)
+print(y)
+
+z = (6+7)
+print(z)
+
+
+min = 5
+second = 60
+second5 = min * second
+print(second5)
+
+min = 2
+sekond = 60
+sekond2 = min * sekond
+print (sekond2)
+
+age = 65
+day = 365
+days = age * day
+print(days)
+
+age = 0
+day = 365
+days1 = age * day
+print(days1)
+
+age = 20
+days = 365
+days2  = age * day 
+
+x = 60
+second = (60 * x + 3 * x)
+print(second)
+
+x = 60 
+second2 = (2 *(60 * x) + 0 * x)
+print(second2)
+
+
+z = 44 % 2 == 0
+s = 61 % 2 == 0
+print (z,s)
