1f884070cd574601108f4572e344b2bc145afdb6
almastasoyan <asoyan.almast@gmail.com>
2023-03-23 23:42:27 +0400

    Added lesson6.py

--- Week4/lesson6.py
+++ Week4/lesson6.py
@@ -4,46 +4,46 @@ print(z)
 
 
 
-# x = True
-# y = (str(x))
-# print(isinstance(y,str))
+x = True
+y = (str(x))
+print(isinstance(y,str))
 
 
 
-# person=input('Enter name ->')
-# x= "Luke, I am your father" 
-# y = 'Luke, I am your sister'
-# z = 'Luke, I am your brother in law' 
-# i = "Biiib biib biib"
-# print(x * (person == "Darth Vader") + y * (person == 'Leia') + z * (person == 'Han')+ i * (person=="R2D2"))
+person=input('Enter name ->')
+x= "Luke, I am your father" 
+y = 'Luke, I am your sister'
+z = 'Luke, I am your brother in law' 
+i = "Biiib biib biib"
+print(x * (person == "Darth Vader") + y * (person == 'Leia') + z * (person == 'Han')+ i * (person=="R2D2"))
 
 
-# x = "Bob Jane"
-# y = "something"
-# print(y, x)
+x = "Bob Jane"
+y = "something"
+print(y, x)
 
 
-# x = "Celebration"
-# y =(x.count('a'))+ (x.count('e')) + (x.count('i')) + (x.count('o'))+(x.count('u'))
-# print(y)
+x = "Celebration"
+y =(x.count('a'))+ (x.count('e')) + (x.count('i')) + (x.count('o'))+(x.count('u'))
+print(y)
 
 
-# x = eval("13 > 44 > 33 > 1")
-# print(x)
+x = eval("13 > 44 > 33 > 1")
+print(x)
 
 
 
-# x = 'minnie mouse'
-# y = '?'
-# z = (x.replace("a", y)).replace("e",y).replace("i",y).replace("o",y).replace("u",y)
-# print(z)
+x = 'minnie mouse'
+y = '?'
+z = (x.replace("a", y)).replace("e",y).replace("i",y).replace("o",y).replace("u",y)
+print(z)
 
 
-# x = "5426137894562147"
-# y = int(x) % 10000
-# z = (len(x)-4)* "*"+ str(y)
-# print(z)
+x = "5426137894562147"
+y = int(x) % 10000
+z = (len(x)-4)* "*"+ str(y)
+print(z)
 
 
-# x = "1234"
-# print((len(x) == 4 and 6) and (x.isdigit()) and ( not ' ' in x))
+x = "1234"
+print((len(x) == 4 and 6) and (x.isdigit()) and ( not ' ' in x))
